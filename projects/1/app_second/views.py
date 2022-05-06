85b2fc9dd5014d9a349237a31532e938f72ba008
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-05-06 20:16:59 +0600

    update

--- projects/1/app_second/views.py
+++ projects/1/app_second/views.py
@@ -42,9 +42,30 @@ def origin_home(request):
 
 
 def todo_detail(request):
-    context = {
+    is_completed = False
+    if is_completed:
+        pass
+    else:
+        pass
 
-    }
+    list = [1, 2, 3]
+    for i in list:
+        index = list.index(i)
+        print(i)
+
+    context = {"title": "Покормить кота!", "description": "ОПИСАНИЕ: is simply dummy text of the printing and "
+                                                          "typesetting "
+                                                          "industry. Lorem Ipsum has been the industry's standard "
+                                                          "dummy text ever since the 1500s, when an unknown printer "
+                                                          "took a galley of type and scrambled it to make a type "
+                                                          "specimen book. It has survived not only five centuries, "
+                                                          "but also the leap into electronic typesetting, "
+                                                          "remaining essentially unchanged. It was popularised in the "
+                                                          "1960s with the release of Letraset sheets containing Lorem "
+                                                          "Ipsum passages, and more recently with desktop publishing "
+                                                          "software like Aldus PageMaker including versions of Lorem "
+                                                          "Ipsum.",
+               "list": [9, 2, 4, 4, 6, 7], "is_completed": True}
     return render(request, 'app_second/pages/DetailTodo.html', context)
 
 
