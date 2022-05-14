05c7f218187c8c97d18e72205fd22d63d1e27a7d
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-05-14 13:47:49 +0600

    Update views.py

--- projects/3/app_teacher/views.py
+++ projects/3/app_teacher/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from . import models
 
 
@@ -30,72 +29,19 @@ def todo_detail(request, todo_id):
 
 
 def todo_list(request):
-    print(request)
-    if request.method == "GET":
-        print("это GET запрос")
-    if request.method == "POST":
-        print("это POST запрос")
-    if request.method == "PUT":
-        print("это PUT запрос")
-    if request.method == "DELETE":
-        print("это DELETE запрос")
-
-    # print("request.method: ", request.method)
-    # print("request.path: ", request.path)
-    # print("request.headers: ", request.headers)
-    # print("request.META: ", )
-
-    # пробегаемся по ключам словаря
-    # for key, value in request.META.keys():
-
-    # пробегаемся по значениям словаря
-    # for key, value in request.META.values():
-
-    # пробегаемся по парам: ключ-значение
-    # for key, value in request.META.items():
-    #     print(f"{key}: {value}")
-    # print("request.data: ", request.data)
-    # print("request.GET: ", request.GET)
-
-    is_completed = False
-    if is_completed:
-        pass
-    else:
-        pass
-
-    list = [1, 2, 3]
-    for i in list:
-        index = list.index(i)
-        # print(i)
-        pass
-
     obj = models.Task.objects.all()
-
-    print(f"obj: {obj}")
-    print(f"obj count: {obj.count()}")
-
     context = {"list": obj}
-
     return render(request, 'app_teacher/pages/todo_list.html', context)
 
 
 def todo_create(request):
-    print("todo_create")
     if request.method == "POST":
-        print("это POST запрос")
-
-        # вызывается Exception (исключение)
-        # title = request.POST["title"]
         title1 = request.POST.get("title", "заголовок по умолчанию")
         description1 = request.POST.get("description", "описание по умолчанию")
-
         obj = models.Task.objects.create(
             title=title1,
             description=description1
         )
         obj.save()
-
-
-        # приём и обработка данных
     context = {}
     return render(request, 'app_teacher/pages/CreateTodo.html', context)
